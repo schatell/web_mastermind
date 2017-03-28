da431d6820ef4e9da8e2e161a8ae359262e10883
schatell <sebastien.chatelle.blanchard@gmail.com>
2017-03-28 17:50:25 -0400

    Completed instruction box

--- public/javascript.js
+++ public/javascript.js
@@ -1,8 +1,13 @@
 $(document).ready(
   function(){
     $("#how_to").click(function(){
-      $("#instruction").animate({
+      $('#instruction').animate({
         left: $("#instruction").parent().width()/2 - $("#instruction").width()/2
+      }, 550);
+    });
+    $('.logo').click(function(){
+      $('#instruction').animate({
+        left: "-2000px"
       }, 550);
     });
   }
