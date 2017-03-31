450772901b5c78004fca0661bf36cef723e5920b
schatell <sebastien.chatelle.blanchard@gmail.com>
2017-03-30 22:08:28 -0400

    Worked on back arrow

--- public/javascript.js
+++ public/javascript.js
@@ -1,5 +1,5 @@
-$(document).ready(
-  function(){
+var back_button = 'playertype';
+$(document).ready(function(){
     $("#how_to").click(function(){
       $('#instruction').animate({
         left: $("#instruction").parent().width()/2 - $("#instruction").width()/2
@@ -17,18 +17,33 @@ $(document).ready(
       $('#player_type').animate({
         right: "0"
       }, 500);
+      $('#back_arrow').animate({
+        left: $("#back_arrow").parent().width()/2 - $("#back_arrow").width()/2 -20
+      }, 500)
       $('.main_container').css('overflow','hidden');
     });
+
     $('#back_arrow').click(function(){
-      $('#menu').animate({
-        right: "0", left :"0"
-      }, 500);
-      $('#player_type').animate({
-        right: '-2000px'
-      },500,function(){
-        $('.main_container').css('overflow','visible')
-      });
+      if (back_button == 'playertype') {
+        $('#menu').animate({
+          right: "0", left :"0"
+        }, 500);
+        $('#player_type').animate({
+          right: '-2000px'
+        },500,function(){
+          $('.main_container').css('overflow','visible')
+        });
+      }else {
+        $('#code_maker_menu').animate({
+          right: '-2000px'
+        }, 500)
+        $('#player_type').animate({
+          left: '0'
+        }, 500);
+        back_button = 'playertype'
+      };
     });
+
     $('#codemaker').click(function(){
       $('#player_type').animate({
         left: '-2000px'
@@ -36,6 +51,7 @@ $(document).ready(
       $('#code_maker_menu').animate({
         right: '0'
       }, 500);
+      back_button = 'code_maker'
     })
   }
 )
